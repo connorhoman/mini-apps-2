9c392870eecd8ebad3ad7c1647075bdcdda01b47
Connor Homan <connorhoman14@gmail.com>
2019-06-25 15:00:33 -0700

    Recieving 10 search results per query

--- challenge_1/client/src/index.jsx
+++ challenge_1/client/src/index.jsx
@@ -7,15 +7,18 @@ class App extends React.Component {
         super(props);
         this.state = {
             search: '',
-            data: []
+            data: [],
+            begin: 1,
+            end: 10
         }
     }
 
-    componentDidMount() {
+    getData() {
         $.ajax({
-            url: '/events',
+            type: 'GET',
+            url: `/events?q=${this.state.search}&_start=${this.state.begin}&_end=${this.state.end}`,
             success: (data) => {
-                this.setState({data: data.slice(0,10)});
+                this.setState({data: data});
             },
             error: (err) =>  {
                 console.log(err);
@@ -27,10 +30,8 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Historical Events Finder</h1>
-                <form>
-                    <input placeholder='Search' onChange={(e) => this.setState({search: e.target.value})}></input>
-                    <button>Go!</button>
-                </form>
+                <input placeholder='Search' onChange={(e) => this.setState({search: e.target.value})}></input>
+                <button onClick={this.getData.bind(this)}>Go!</button>
                 <div>
                     {this.state.data.map((item, index) => {
                         return <div key={index}><span style={{fontWeight: '900', marginRight: '10px'}}>{item.date}</span>{item.description}</div>
