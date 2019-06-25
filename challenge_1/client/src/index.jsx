012a4f142538f14179c06195ba936066197355da
Connor Homan <connorhoman14@gmail.com>
2019-06-25 14:30:24 -0700

    Rendering data from server

--- challenge_1/client/src/index.jsx
+++ challenge_1/client/src/index.jsx
@@ -6,13 +6,38 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            search: '',
+            data: []
         }
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: '/events',
+            success: (data) => {
+                this.setState({data: data.slice(0,10)});
+            },
+            error: (err) =>  {
+                console.log(err);
+            }
+        });
     }
 
     render() {
         return (
-            <div>Hello World</div>
-        )
+            <div>
+                <h1>Historical Events Finder</h1>
+                <form>
+                    <input placeholder='Search' onChange={(e) => this.setState({search: e.target.value})}></input>
+                    <button>Go!</button>
+                </form>
+                <div>
+                    {this.state.data.map((item, index) => {
+                        return <div key={index}><span style={{fontWeight: '900', marginRight: '10px'}}>{item.date}</span>{item.description}</div>
+                    })}
+                </div>
+            </div>
+        );
     }
 }
 
